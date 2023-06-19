b3c4023ed786910b18cc007401e055b17278c5c9
OtakeYuto <yuto.programming@gmail.com>
2023-06-20 06:06:22 +0900

    change ALLOWED_HOSTS

--- pdf_converter/settings.py
+++ pdf_converter/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ynew(+hek&ou(@bhpdydtu-4fgl4=c4ptni-3mfg*+mnccdp4_'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
